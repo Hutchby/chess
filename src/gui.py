2886f785575802be857daef9c93f27ba72e0ea45
paul <paul.halbeher@gmail.com>
2016-06-11 13:10:58 +0200

    un début de code

--- src/gui.py
+++ src/gui.py
@@ -1,1 +1,27 @@
 # rendu graphique pour le moment ta fonction de rendu
+
+from tkinter import *
+
+def afficherTerrain(listPiece):
+   window = Tk()
+   window.title("Plateau de jeu")
+   canvas = Canvas(window, width=800, height=800)
+   canvas.create_text(45, 755, text = '0')
+   canvas.create_text(755, 675, text = '0')
+
+   for i in range(1, 8):
+       canvas.create_line(90 * i, 0, 90 * i, 720)
+       canvas.create_text(i * 90 + 45, 755, text = i)
+       canvas.create_text(755, 720 - i * 90 - 45, text = i)
+       canvas.create_line(0, 90 * i, 720, 90 * i)
+
+   for piece in listPiece:
+       if (piece.player == 1):
+           color = "blue"
+       else:
+           color = "red"
+       canvas.create_text(piece.position[0] * 90 + 45, 720 - piece.position[1] * 90 - 45, text = piece.category, fill= color)
+
+   canvas.pack()
+   window.mainloop()
+
--- src/pieces.py
+++ src/pieces.py
@@ -1,3 +1,25 @@
 # TODO: 7 classes de piece, une original et 6 qui en hérite, attribut coordonée et 2
 # fonction de mouvement, une pour les possibilité pour l'ia et l'autre pour
-# verifier que le cout est possible
+# verifier que le coup est possible
+
+class Piece: #definition classe piece
+    """ Classe définissant une piece caractérisée par :
+
+            - sa position
+            - son type (categorie)
+            - son propriétaire
+      """
+    nbPieceP = [0, 0]
+
+    def __init__(self): #constructeur
+        self.position = [0, 0]
+        self.value = 0
+        self.player = 1
+
+    def __init__(self, position, value, player): #constructeur
+        self.position = position
+        Piece.nbPieceP[player - 1] += 1
+        self.player = player
+
+
+
