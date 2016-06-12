2a3f8cd9d46843c89b78ecec29883f509291d634
paul <paul.halbeher@gmail.com>
2016-06-12 23:10:28 +0200

    classe mises a jour il manque la fonction deplacement debut de isCheck

--- src/main.py
+++ src/main.py
@@ -3,7 +3,24 @@
 
 # gestion globale, variables globales et autres
 
+from src.pieces import *
+from src.gui import *
+from src.rules import *
+from src.ai import *
+
 print("Hello World")
-run = False
-while run:
-    continue
+
+listPiece = newGame()
+
+afficherTerrain(listPiece)
+
+print(len(listPiece))
+
+print(listPiece[1].symbol)
+print(listPiece[1].player)
+
+
+for i in range(8,0):
+    print(i)
+
+print(fonctionScore(listPiece, 1))
--- src/rules.py
+++ src/rules.py
@@ -1,1 +1,36 @@
 # Je suis pas sur que celui la soit necessaire
+
+from src.pieces import *
+
+def isCheck(listPiece, player):
+    for piece in listPiece:
+        x = piece.position[0]
+        y = piece.position[1]
+        if piece is King:
+            if piece.player == player:
+                for i in range(x, 8):
+                    for piece2 in listPiece:
+                        if piece2.position[0] == i & piece2.position[1] == y:
+                            if piece2 is Queen or piece2 is Rook:
+                                return True
+                            else:
+                                break
+
+
+    return False
+
+def newGame():
+
+    listPiece = []
+    for i in [-1, 1]:
+        for j in [-1, 1]:
+            listPiece.append(Rook([int(3.5 + j*3.5), int(3.5 + i*3.5)], int((3+i)/2)))
+            listPiece.append(Knight([int(3.5 + j*2.5), int(3.5 + i*3.5)], int((3+i)/2)))
+            listPiece.append(Bishop([int(3.5 + j*1.5), int(3.5 + i*3.5)], int((3+i)/2)))
+
+        for j in range(0,8):
+            listPiece.append(Pawn([j, int(3.5 + i*2.5)], int((3+i)/2)))
+
+        listPiece.append(Queen([int(3.5 + i*0.5), int(3.5 + i*3.5)], int((3+i)/2)))
+        listPiece.append(King([int(3.5 - i*0.5), int(3.5 + i*3.5)], int((3+i)/2)))
+    return listPiece
