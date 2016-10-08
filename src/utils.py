7bf690da37ed4416757b370ad7eed43a36fc9cdd
paul <paul.halbeher@gmail.com>
2016-10-08 16:39:31 +0200

    so much new features, pina colada mojito added

---
+++ src/utils.py
@@ -0,0 +1,19 @@
+from copy import deepcopy
+from src.rules import *
+from src.gui import *
+from src.pieces import *
+from src.ai import *
+
+def max_indice(list):
+    maxi = 0
+    for i in list:
+        if list[i] > list[maxi]:
+            maxi = i
+    return maxi
+
+def max_indice(list):
+    mini = 0
+    for i in list:
+        if list[i] < list[mini]:
+            mini = i
+    return mini
