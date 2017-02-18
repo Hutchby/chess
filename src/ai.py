19c0f04991017884e51260aeb7e54616d3fd7310
brw9293 <paulderueil@hotmail.fr>
2017-02-18 17:23:15 +0100

    fixed viral_spread

--- src/ai.py
+++ src/ai.py
@@ -1,8 +1,11 @@
+# TODO: check function score
+
 from src.pieces import *
 from src.utils import *
 import random
 
 
+# to be improved
 def function_score(pieces, player):
     """ donne un score en fonction des pieces et de la distance au centre afin de pousser les pieces a se combattre
     Le controle du centre est important aux echecs sauf si il s’agit du roi.
@@ -36,21 +39,18 @@ def random_move(pieces, player):
 
 
 def viral_spread(pieces, player, nb_try, diff=3):
-    list_move = list_all_move(pieces, player)
-    list_score = [0] * len(list_move)
-    for i in range(0, len(list_move)):
+    list_move = random.sample(list_all_move(pieces, player), nb_try)
+    list_score = [0] * nb_try
+    for i in range(0, nb_try):
         first_move = list_move[i]
-        try_move(pieces, player, first_move)
         pieces_temp = deepcopy(pieces)
         pieces_temp[first_move[1]] = pieces_temp.pop(first_move[0])
-        for t in range(0, nb_try):
-            current_player = player
-            for s in range(0, diff):
-                current_player *= -1
-                move = random_move(pieces_temp, current_player)
-                pieces_temp[move[1]] = pieces_temp.pop(move[0])
-            list_score[i] += function_score(pieces_temp, player)
-            # list_score[i] = function_score(pieces_temp, player)
+        current_player = player
+        for t in range(0, diff):
+            current_player *= -1
+            move = random_move(pieces_temp, current_player)
+            pieces_temp[move[1]] = pieces_temp.pop(move[0])
+        list_score[i] = function_score(pieces_temp, player)
 
     # list_nb_move = [0] * len(list_move)
     move = list_move[max_index(list_score)]
@@ -120,4 +120,4 @@ def main_ia(player, pieces, ia_type, difficulty):
     else:
         print("error not the right IA selected")
         move = ""
-    return move
+    dict_pieces[move[1]] = dict_pieces.pop(move[0])
